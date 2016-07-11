36a2a63d8e4b37ff6863724df09c60a3b39facb3
Erick Lavoie <github@ericklavoie.com>
2016-07-11 14:03:40 +0200

    mc: Modified to use the custom pseudo-random number generator

--- benchmarks/mc/implementations/matlab-mixed/runner.m
+++ benchmarks/mc/implementations/matlab-mixed/runner.m
@@ -1,8 +1,6 @@
 function runner(n)
     % feature accel off;
-    rng(100);
-    %rand("seed", 100);
-    rdata = rand(2,n);
+    rdata = createMatrixRandJS(2,n);
     n = 10;
     tic();
     piValue = MonteCarlo(rdata);
